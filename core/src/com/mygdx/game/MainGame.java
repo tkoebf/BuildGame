22e72ba8d72ae6d36f54c5516e1b937f34d15123
Snowtakeo <mafo2306@web.de>
2017-10-22 17:05:47 +0200

    Update

--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -2,25 +2,39 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Camera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 	}
+	
+	public void resize(int width, int height) {
+		camera.viewportWidth = (float)width;
+		camera.viewportHeight = (float)height;
+        camera.update();
+    }
 
 	@Override
 	public void render () {
+		camera.rotate(camera.direction, 1f);
+		camera.update();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.setProjectionMatrix(camera.combined);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.mygdx.game.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Best Game ever!";
 		new LwjglApplication(new MainGame(), config);
 	}
 }
