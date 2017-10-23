051a00f7779826c2629b45364b0737ac1c0fc557
Janek <janekx21@gmail.com>
2017-10-23 19:13:07 +0200

    d

--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import javax.management.Query;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
@@ -11,28 +13,46 @@ import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
+import com.badlogic.gdx.graphics.g3d.decals.Decal;
+import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
+import com.badlogic.gdx.graphics.g3d.decals.DecalMaterial;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class MainGame extends ApplicationAdapter {
 	public static SpriteBatch batch;
+	public static ModelBatch models;
 	Texture img;
 	public static Camera camera;
 	public static TextureAtlas atlas;
+	
+	public static DecalBatch decalBatch;
+	public static CameraGroupStrategy cameraGroupStrategy;
+	
+	float FOV = 90;
 	
 	@Override
 	public void create () {
 		
 		batch = new SpriteBatch();
+		models = new ModelBatch();
+		
 		img = new Texture("badlogic.jpg");
 		//camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		camera = new PerspectiveCamera(90,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		camera = new PerspectiveCamera(FOV,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		camera.translate( new Vector3(0,0,20));
 		camera.far = 100;
 		camera.rotate(Vector3.X,30);
+		camera.rotate(Vector3.Z,45);
+		
+		cameraGroupStrategy  = new CameraGroupStrategy(camera);
+		decalBatch = new DecalBatch(cameraGroupStrategy);
 		atlas = new TextureAtlas("sprites.txt");
 		
-		new Building(0,0);
+		//new Building(0,0);
 		
 		Gdx.input.setInputProcessor(new InputAdapter(){
 			@Override
@@ -41,6 +61,10 @@ public class MainGame extends ApplicationAdapter {
 				System.out.println(mpos);
 				new Building(mpos.x,mpos.y);
 
+				return true;
+			}
+			@Override
+			public boolean scrolled(int amount) {
 				return true;
 			}
 		});
@@ -77,11 +101,12 @@ public class MainGame extends ApplicationAdapter {
 		camera.translate(mov);
 		//camera.rotate(camera.direction,Gdx.input.getDeltaY());
 		camera.update();
+		
 		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.setProjectionMatrix(camera.combined);
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		
 		for(Building building:Building.allbuildings.values()){
 			building.update();
@@ -91,11 +116,37 @@ public class MainGame extends ApplicationAdapter {
 		}
 		
 		batch.end();
+		
+		Decal de = Decal.newDecal(100f,100f, new TextureRegion(img),true);
+		
+		de.setRotationX(0);
+		de.setDimensions(10, 10);
+		de.setBlending(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		//de.setColor(1, 1, 1, 1);
+		de.setPosition(0, 0,-20);
+		
+		//cameraGroupStrategy  = new CameraGroupStrategy(camera);
+		decalBatch.setGroupStrategy(cameraGroupStrategy);
+		
+		models.begin(camera);
+		models.end();
+		
+		decalBatch.add(de);
+		de.setPosition(20, 0, 0);
+		decalBatch.add(de);
+		de.setPosition(0, 20, 0);
+		decalBatch.add(de);
+		decalBatch.flush();
+		
+		//System.out.println(de.getVertices());
+		
+		
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		decalBatch.dispose();
 	}
 }
